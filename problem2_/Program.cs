2c90261261c412f0101b998120fd73ec7b72ba9f
Hovhannes <hovhannes.vardanyan.53@gmail.com>
2019-11-09 18:21:42 +0400

    Without Ref

---
+++ problem2_/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = new int[] { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("The old array is");
+            PrintArray(inputArray);
+            Console.WriteLine("Enter the index");
+            if (int.TryParse(Console.ReadLine(), out int result) && result >= 0 && result < inputArray.Length)
+            {
+                int targetIndex = result;
+                DeleteMember(inputArray, targetIndex);
+                Console.WriteLine("New Array is ");
+                PrintArray(inputArray);
+            }
+            else
+            {
+                Console.WriteLine("You've entered wrong symbol");
+            }
+
+            Console.ReadKey();
+        }
+
+        static void PrintArray(int[] inputArray)
+        {
+            int length = inputArray.Length;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine(inputArray[i]);
+            }
+
+        }
+        static void DeleteMember(int[] inputArray, int targetIndex)
+        {
+            int[] newArray = new int[inputArray.Length];
+            int indexofNewArr = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (i != targetIndex)
+                {
+                    newArray[indexofNewArr] = inputArray[i];
+                    indexofNewArr++;
+                }
+                else
+                {
+                    newArray[indexofNewArr] = 0;
+                    indexofNewArr++;
+                }
+            }
+
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                inputArray[i] = newArray[i];
+            }
+
+        }
+
+    }
+}
